300a6b24b748dda733bd27d8e2579e39dda0a306
JonasIFPB <jonas.sarmento@academico.ifpb.edu.br>
2024-08-12 21:52:51 -0300

    Update para_numeros_reais.py
    finalizando primeira etapa

--- IEEE-754/para_numeros_reais.py
+++ IEEE-754/para_numeros_reais.py
@@ -72,7 +72,7 @@ if tipo == '1':
         decimalBinario += str(parte_inteira_decimal)
 
         mult = parte_fracionaria_decimal * 2
-    print(decimalBinario)
+    
     ##formatando a parte decimal 
     #decimalLista = []
     #for i in decimalBinario:
@@ -87,29 +87,80 @@ if tipo == '1':
 
 
 
-numeroConvertidoNFormatado = inteiroBinario + ',' + decimalBinario
-numeroConvertido = inteiroBinario + decimalBinario
+    numeroConvertidoNFormatado = inteiroBinario + ',' + decimalBinario
+    numeroConvertido = inteiroBinario + decimalBinario
 
 
-if inteiro > 0:
-    numeroConvertido = numeroConvertido[0] + ',' + numeroConvertido[1::]
+    if inteiro > 0:
+        numeroConvertido = numeroConvertido[0] + ',' + numeroConvertido[1::]
 
 
-else:
-    for i in range(len(numeroConvertido)):
-        if numeroConvertido[i] == '1':
-            numeroConvertido = numeroConvertido[i] + ',' + numeroConvertido[i+1::]
-            break
+    else:
+        for i in range(len(numeroConvertido)):
+            if numeroConvertido[i] == '1':
+                numeroConvertido = numeroConvertido[i] + ',' + numeroConvertido[i+1::]
+                break
 
-#achando o valor da notação
-if numeroConvertidoNFormatado.find(',') < numeroConvertidoNFormatado.find('1'):
-    expoente = (numeroConvertidoNFormatado.find(',') - numeroConvertidoNFormatado.find('1')) 
+    #achando o valor da notação
+    if numeroConvertidoNFormatado.find(',') < numeroConvertidoNFormatado.find('1'):
+        expoente = (numeroConvertidoNFormatado.find(',') - numeroConvertidoNFormatado.find('1')) 
 
-else:
-    expoente = (numeroConvertidoNFormatado.find(',') - numeroConvertidoNFormatado.find('1')) - 1
+    else:
+        expoente = (numeroConvertidoNFormatado.find(',') - numeroConvertidoNFormatado.find('1')) - 1
 
-mantissa = numeroConvertido[2::]
-print(numeroConvertidoNFormatado)
-print(expoente)
-print(numeroConvertido)
-print(mantissa)
+    mantissa = numeroConvertido[2::]
+
+    #exposente mais 127
+
+    expoente = int(expoente) + 127
+
+    #convertendo para binário com 8 bits
+
+    inteiroExpoente = ""
+    if expoente == 1:
+        inteiroExpoente += '1'
+    else:
+        restoExpoente = expoente % 2
+        quocienteExpoente = expoente // 2
+        inteiroExpoente += str(restoExpoente)
+        while True:
+            if quocienteExpoente == 1:
+                break
+            restoExpoente = quocienteExpoente % 2
+            quocienteExpoente = quocienteExpoente // 2
+            inteiroExpoente += str(restoExpoente)
+        inteiroExpoente += str(quocienteExpoente)  
+        inteiroExpoente = inteiroExpoente[::-1]
+    print(len(inteiroExpoente))
+    print(inteiroExpoente)
+    print(len(mantissa))
+    print(mantissa)
+
+    if len(inteiroExpoente) < 8:
+        for i in range(8 - len(inteiroExpoente)):
+            inteiroExpoente += '0'
+    
+   
+    if len(mantissa) < 23:
+        for i in range(23 - len(mantissa)):
+            mantissa += '0'
+  
+
+    
+    if sinal == '+':
+        sinal = "0"
+    else:
+        sinal = "1" 
+
+    resultado = sinal + inteiroExpoente + mantissa
+
+
+
+
+
+print(f'''
+
+Número Convertido :   {resultado}
+
+
+''')
